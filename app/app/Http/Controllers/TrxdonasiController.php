999126d5484decba4f5b8e20e6cb2c98132694dd
toni tegar sahidi <tonitegarsahidi@gmail.com>
2019-01-21 16:21:00 +0700

    fixing

---
+++ app/app/Http/Controllers/TrxdonasiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Trxdonasi;
+use Illuminate\Http\Request;
+
+class TrxdonasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Trxdonasi  $trxdonasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trxdonasi $trxdonasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Trxdonasi  $trxdonasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trxdonasi $trxdonasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Trxdonasi  $trxdonasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trxdonasi $trxdonasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Trxdonasi  $trxdonasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trxdonasi $trxdonasi)
+    {
+        //
+    }
+}
